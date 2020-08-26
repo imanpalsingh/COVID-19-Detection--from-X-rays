169080fab5ab44ca0e833400366aaaa881c8f2b5
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2020-08-26 20:47:45 +0530

    Added additional metadata saving and renaming of images

--- src/data/extract.py
+++ src/data/extract.py
@@ -2,13 +2,17 @@
 Author: Imanpal Singh <imanpalsingh@gmail.com>
 Location:  src.data
 Date Created: 25-08-2020
-Date Modified: 25-08-2020
+Date Modified: 26-08-2020
 '''
 
 '''
 
 Change Logs
 ===========
+
+1) 26-08-20
+
+Added additional metada saving and renaming of images
 
 None
 
@@ -34,9 +38,19 @@ class Organizer:
     Specifies the path of the location of the dataset. The dataset should represent the same structure as of the repository 
     `https://github.com/ieee8023/covid-chestxray-dataset` of the original dataset.
 
+    -----------------------------------
+
     `images_only` : `bool`
 
     Specifies whether to save the demographics given in `path/metadata.csv` along with the images if `False` or only images if `True`
+
+    -----------------------------------
+
+    `reaname_files`  : `bool`
+
+    Specifies if the files that are being copied (images) should be renamed.
+    The images will be renamed for example as  `Covid191.png`
+
 
     Example
     =======
@@ -49,19 +63,17 @@ class Organizer:
 
     '''
 
-    def __init__(self,path: str, images_only : bool = True) -> None:
+    def __init__(self,path: str, images_only : bool = True, rename_files : bool = True) -> None:
 
     
         if not os.path.isdir(path):
 
             raise FileNotFoundError(f"The Directory '{path}' does not exist.")
 
-        if not images_only:
-
-            raise NotImplementedError("'images_only = False' is not yet supported")
 
         self.path = path
         self.images_only = images_only
+        self.rename_files = rename_files
 
 
     def start(self,output_dir: str) -> None :
@@ -94,9 +106,16 @@ class Organizer:
             raise FileNotFoundError(f"The Directory '{output_dir}' does not exist.")
 
         metadata = pd.read_csv(self.path + 'metadata.csv')
+
         classes = metadata['finding'].unique()
         
+
         for folder in classes:
+
+            '''
+            For each unique class given in the metadata.csv create a folder for the class if it doesn't exist
+
+            '''
             
             path = os.path.join(output_dir,folder)
             try:
@@ -105,15 +124,34 @@ class Organizer:
             except FileExistsError:
                 pass
 
-            files = metadata[metadata['finding'] == folder]['filename']
-            
+            data = metadata[metadata['finding'] == folder]
+            files = data['filename']
+            locations = data['folder']
 
-            for _file in files:
+
+            for index, (_file,location) in enumerate(zip(files,locations)):
+
+                '''
+                For each file that belongs to the class copy it from its current position to the newly created
+                folder for that class
+
+                '''
+                if self.rename_files:
+                        output_filename = folder + str(index) + os.path.splitext(_file)[1]
+                    
+                else:
+                        output_filename = _file 
                 
                 try:
-                    copyfile(self.path + '/images/' + _file, output_dir + '/' + folder + '/' + _file)
+                    
+                    copyfile(self.path + '/' + location + '/' + _file, output_dir + '/' + folder + '/' + output_filename)
 
                 except FileNotFoundError:
 
-                    print(f"Warning! : Skipping {_file} as it was not found")
+                    print(f"Warning! : Skipping '{_file}' as it was not found at location '{location}'")
 
+            
+            if not self.images_only:
+
+                data.to_csv(output_dir + '/' + folder + '/' + folder + '.csv', index = False)
+             
