dff5fe1cd8480fe16f02fbb1c23b9002bfcfc9d4
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2020-09-01 22:14:56 +0530

    Added helper class for splitting of data

--- src/data/extract.py
+++ src/data/extract.py
@@ -2,7 +2,7 @@
 Author: Imanpal Singh <imanpalsingh@gmail.com>
 Location:  src.data
 Date Created: 25-08-2020
-Date Modified: 26-08-2020
+Date Modified: 01-09-2020
 '''
 
 '''
@@ -10,7 +10,16 @@ Date Modified: 26-08-2020
 Change Logs
 ===========
 
-1) 26-08-20
+1) 01-09-20
+
+Added Class to allow automated splitting of training and testing data for easier keras image integration  
+
+2) 27-08-20
+
+Changed class name Organizer to Raw
+Added class TrainValTest to split the images into folders of training and test
+
+2) 26-08-20
 
 Added additional metadata saving and renaming of images
 
@@ -19,8 +28,10 @@ Added additional metadata saving and renaming of images
 import pandas as pd 
 import os
 from shutil import copyfile
+import glob
+from random import shuffle
 
-class Organizer:
+class Raw:
 
     '''
 
@@ -91,8 +102,8 @@ class Organizer:
         =======
 
         ```
-        >>>from data.extract import Organizer
-        >>>obj = Organizer(path = "dataset/")
+        >>>from data.extract import Raw
+        >>>obj = Raw(path = "dataset/")
         >>>obj.start(output_dir="dataset/output/")
         ```
 
@@ -100,7 +111,8 @@ class Organizer:
 
         if not os.path.isdir(output_dir):
 
-            raise FileNotFoundError(f"The Directory '{output_dir}' does not exist.")
+            print(f"Warning! : The Directory '{output_dir}' does not exist and has been created.")
+            os.mkdir(output_dir)
 
         metadata = pd.read_csv(self.path + 'metadata.csv')
 
@@ -152,3 +164,136 @@ class Organizer:
 
                 data.to_csv(output_dir + '/' + folder + '/' + folder + '.csv', index = False)
              
+
+class TrainTest:
+
+    '''
+    Class for automated splitting of training and testing data into two different folders so that keras's ImageDataGenerator
+    can be easily applied.
+
+    Inputs
+    ======
+
+    `path` : `str`
+
+    Path were class separated images are stored. This is obtained by executing the `Raw` class first on the raw dataset. 
+
+    ------------------
+
+    `train_perc` : `float`
+
+    A number between 0 and 1 defining the percentage of total images to be used for training and rest for testing.
+
+    Note :  To avoid class left out problem percentage is calculated from each class separately so that every class 
+    is present in both training and test. However, still a class can be left out if the percentage is high and data is low enough
+
+    ---------------------
+
+    `shuffle` : `bool`
+
+    If to randomly select images for training and testing . If set to `False` first `total length x train_perc` images/data are selected
+
+
+    Example
+    =======
+
+    ```
+    >>>from data.extract import TrainTest
+    >>>obj = Test(path = "dataset/output/",train_perc=0.9)
+
+    ```
+    '''
+
+    def __init__(self, path, train_perc = 0.5, shuffle = False):
+
+        self.path = path
+        self.train_perc = train_perc
+        self.shuffle = shuffle
+    
+    def start(self,output_dir):
+
+        '''
+
+        Function to start the process of splitting of data into training and test set
+
+        Input
+        =====
+
+        `output_dir` : `str`
+
+        Specifies the path to the folder in which two folders will be created `training` and `testing` 
+        and files will be copied to these two folders
+
+        Example
+        =======
+
+        ```
+        >>>from data.extract import TrainTest
+        >>>obj = Test(path = "dataset/output/",train_perc=0.9)
+        >>>obj.start(output_dir="dataset/final/")
+        ```
+
+        '''
+
+
+        folders = os.listdir(self.path)
+
+        if not os.path.isdir(output_dir):
+
+            print(f"Warning! : The Directory '{output_dir}' does not exist and has been created.")
+            os.mkdir(output_dir)
+
+        try:
+
+            os.mkdir(output_dir + '/training/')
+
+        except FileExistsError:
+
+            pass
+        
+        try:
+
+            os.mkdir(output_dir + '/testing/')
+        
+        except FileExistsError:
+
+            pass
+
+
+        
+        for folder in folders:
+
+            files = os.listdir(self.path+folder)
+
+            if self.shuffle:
+
+                shuffle(files)
+
+            total_count = len(files)
+
+            training_till = int(total_count*self.train_perc)
+
+            train_files = files[:training_till]
+            test_files = files[training_till:]
+
+            for train in train_files:
+
+                try:
+                    
+                    copyfile(self.path + '/' + folder + '/' + train, output_dir + '/training/' + train)
+                
+                except FileNotFoundError:
+
+                    print(f"Warning! : Skipping '{train}' as it was not found at location '{output_dir + '/' + folder + '/'}'")
+
+            for test in test_files:   
+
+                try:
+                    
+                    copyfile(self.path + '/' + folder + '/' + test, output_dir + '/testing/' + test)
+                
+                except FileNotFoundError:
+
+                    print(f"Warning! : Skipping '{train}' as it was not found at location '{output_dir + '/' + folder + '/'}'")
+
+            
