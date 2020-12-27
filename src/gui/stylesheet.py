fee433822e1dc68c9d9a16194b0d8eaee66ec993
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2020-12-28 03:40:58 +0530

    Centerlized qcss

---
+++ src/gui/stylesheet.py
@@ -0,0 +1,46 @@
+
+cnormal = ''' QMainWindow {border: 1px solid black;
+             border-radius : 4px; 
+             background-color :  #212121;}
+                              
+        '''
+cWidget = ''' QWidget{border: 1px solid black; border-radius : 4px; background-color :  #212121;}
+                           '''
+dirText = ''' 
+        border-right : none;
+        border-top-right-radius: 0px;
+        border-bottom-right-radius: 0px; '''
+
+head = '''font-size : 25px;
+         border: 1px solid white;
+         border-top:hidden;
+         border-left:hidden;
+         border-right:hidden;
+         border-radius:none;
+         '''
+
+dirButton = '''
+        background-color:transparent;
+        font-size : 14px;
+        border : 1px solid white; 
+        border-top-left-radius: 0px;
+        border-bottom-left-radius: 0px;       
+         '''
+
+conform = '''
+        background-color:transparent;
+        font-size : 14px;
+        border : 1px solid white;  
+        '''
+
+cexit = '''
+        background-color:transparent;
+        border : none;
+        font-size : 14px;
+        border : 1px solid white;   
+         '''
+
+windowHeader =  '''font-size : 14px; border : 1px solid white; border-top : none; border-left : none;
+         border-right : none; 
+         border-color: #111;
+         border-radius : 0px; '''
