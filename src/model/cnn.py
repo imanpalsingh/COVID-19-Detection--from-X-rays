daafec91630bcc78952786e9c735979e08ae3f7f
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2020-12-29 01:37:52 +0530

    minor changes

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -16,8 +16,11 @@ from tensorflow.keras import models
 from src.data.generators import create
 from tensorflow import keras
 import datetime
+from matplotlib import pyplot as plt
 
 train, test = create()
+
+## Model ##
 
 model = keras.Sequential()
 model.add(Conv2D(filters=32,kernel_size=4,input_shape=(256,256,3)))
@@ -28,14 +31,16 @@ model.add(Dense(15,activation='relu'))
 model.add(Dense(1,activation='sigmoid'))
 model.summary()
 
+## Callbacks##
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
 early_Stopping = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 
 opt = keras.optimizers.Adam(learning_rate=0.0001)
-model.compile(optimizer=opt,loss='binary_crossentropy',metrics=['accuracy','AUC'])
+model.compile(optimizer=opt,loss='binary_crossentropy',metrics=['accuracy','AUC',keras.metrics.Recall(), keras.metrics.Precision()])
+
 history = model.fit(train,steps_per_epoch=train.samples//8,validation_data=test,validation_steps=test.samples//8,
-epochs=100,callbacks=[tensorboard_callback,early_Stopping])
+epochs=10,callbacks=[tensorboard_callback,early_Stopping])
+
 
 model.save('src/model/saved/covid-19')
--- src/model/transfer.py
+++ src/model/transfer.py
@@ -18,8 +18,11 @@ import datetime
 
 INPUT_SHAPE = (256,256,3)
 
-feature_extraction = Xception(input_shape=INPUT_SHAPE,include_top=False)
+train,test = create()
 
+### Model ###
+
+feature_extraction = Xception(input_shape=INPUT_SHAPE,include_top=False)
 feature_extraction.trainable = False
 
 
@@ -34,15 +37,12 @@ Output = keras.layers.Dense(1,activation='sigmoid')(x)
 model = keras.Model(inputs = Input, outputs = Output)
 model.compile(optimizer=keras.optimizers.Adam(lr=0.0001), loss=keras.losses.BinaryCrossentropy(from_logits=True)
 , metrics=['accuracy','AUC'])
-
 model.summary()
-train,test = create()
 
+## Callbacks
 log_dir = "logs/fitTransfer/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
 early_Stopping = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
-
 
 
 history = model.fit(train,steps_per_epoch=train.samples//8,validation_data=test,validation_steps=test.samples//8,
